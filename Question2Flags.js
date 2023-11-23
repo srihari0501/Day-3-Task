f5e462fced65987afa70b84032fe419ba771cb87
srihari0501 <112402921+srihari0501@users.noreply.github.com>
2023-11-23 11:49:56 +0530

    Update Question2Flags.js

--- Question2Flags.js
+++ Question2Flags.js
@@ -7,7 +7,7 @@ request.onload = function () {
     var result = JSON.parse(request.response);
     for(var a in result)
     {
-        console.log(a,result[a]["flags"]);
+        console.log(a,result[a]["flags"]["png"]);
     }
 }
 
